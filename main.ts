bd59190a1c62f1336ae516d39aff4446c592104f
Whoopper <KKHL1@yvg.vic.edu.au>
2025-03-12 15:12:54 +1100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("" + (randint(1, 13)))
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+basic.forever(function () {
+	
+})
